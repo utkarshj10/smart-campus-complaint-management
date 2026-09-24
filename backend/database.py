6376e67b9779a899cb8e24d029c82cd94668b64f
Utkarsh Jaiswal <utkarshmanishjaiswal@gmail.com>
2026-09-24 21:53:31 +0530

    Connect FastAPI to MongoDB

---
+++ backend/database.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+load_dotenv()
+
+MONGODB_URI = os.getenv("MONGODB_URI")
+
+client = MongoClient(MONGODB_URI)
+
+db = client["smart_campus"]
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from database import db
 
 app = FastAPI(title="Smart Campus Complaint Management System")
 
 
 @app.get("/")
 def home():
-    return { "message": "Smart Campus Complainet Management System API is working" }
+    return {"message": "Smart Campus Complainet Management System API is working"}
+
+
+@app.get("/db-test")
+def database_test():
+    db.command("ping")
+    return {"message": "MongoDB connection successful"}
